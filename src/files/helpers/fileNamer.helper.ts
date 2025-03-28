0a505cd8f0aad31fed3ffe0400bc9a6e5bd33294
renattofarid <renattofaridper@gmail.com>
2025-03-28 00:26:11 -0500

    Actualizar la generación de nombres de archivo para usar la extensión original del archivo

--- src/files/helpers/fileNamer.helper.ts
+++ src/files/helpers/fileNamer.helper.ts
@@ -1,9 +1,11 @@
 import { v4 as uuid } from 'uuid'
+import { extname } from 'path';
 
 export const fileNamer = ( req: Express.Request, file: Express.Multer.File, callback: Function ) => {
     // console.log({ file })
     if ( !file ) return callback( new Error('File is empty'), false );
-    const fileExtension = file.mimetype.split('/')[1];
-    const fileName = `${ uuid() }.${ fileExtension }`;
+    // const fileExtension = file.mimetype.split('/')[1];
+    const fileExtension = extname(file.originalname);
+    const fileName = `${ uuid() }${ fileExtension }`;
     callback(null, fileName );
 }
